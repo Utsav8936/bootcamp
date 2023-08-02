61cdb1d5657b80c5437931bdf26f221bee024aab
Utsav8936 <106107851+Utsav8936@users.noreply.github.com>
2023-08-02 19:43:53 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class demo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int sum=0;
+        for(int i=0;i<5;i++){
+            a=sc.nextInt();
+            sum+=a;
+        }
+        System.out.println(sum);
+        }
+    }
